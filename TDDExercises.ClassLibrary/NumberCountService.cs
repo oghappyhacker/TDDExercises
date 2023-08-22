ffdb0fbe53aa5494a30261fcbe1dd1fe51ce2066
Jordan Graham <oghappyhacker@gmail.com>
2023-08-22 15:58:55 -0500

    Added NumberCountService and Tests

---
+++ TDDExercises.ClassLibrary/NumberCountService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExercises.ClassLibrary
+{
+    public class NumberCountService
+    {
+        public int Count(int numberToCount)
+        {
+            return numberToCount;
+        }
+        public int CountAlt(int numberToCount)
+        {
+            int holdNumber = 0;
+            for (int i = 1;i <= numberToCount;i++)
+                holdNumber++;
+            return holdNumber;
+        }
+    }
+}
---
+++ TDDExercises.Test/NumberCountServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDExercises.ClassLibrary;
+
+namespace TDDExercises.Test
+{
+    public class NumberCountServiceTests
+    {
+        private NumberCountService _ncs;
+        public NumberCountServiceTests()
+        {
+            _ncs = new NumberCountService();
+        }
+        [Theory, InlineData(100, 100)]
+        public void Count_Should_Pass(int numToCount, int expected)
+        {
+            var result = _ncs.Count(numToCount);
+            result.Should().Be(expected);
+        }
+        [Theory,InlineData(100, 100)]
+        public void CountAlt_Should_Pass(int numToCount,int expected)
+        {
+            var result = _ncs.CountAlt(numToCount);
+            result.Should().Be(expected);
+        }
+    }
+}
